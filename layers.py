1e484e39e1d2e162607ea456ae485a838fd730df
QuackingBob <quackingbob101@gmail.com>
2022-12-18 23:26:34 -0800

    update

--- layers.py
+++ layers.py
@@ -66,15 +66,69 @@ class Octree:
                 return None
             return self._search(node.children[index], x, y, z)
 
-
 class PositionalEmbedder(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, embedding_dim: int) -> None:
         super().__init__()
+        self.embedding_dim = embedding_dim
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        pos_emb = torch.zeros(x.size(0), self.embedding_dim)
+        for i in range(self.embedding_dim):
+            if i % 2 == 0:
+                pos_emb[:, i] = x[:, 0] / np.power(10000, i / self.embedding_dim)
+            else:
+                pos_emb[:, i] = x[:, 1] / np.power(10000, (i - 1) / self.embedding_dim)
+        return pos_emb
+
 
 
 class VQAD(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, embedding_dim: int, codebook_size: int) -> None:
         super().__init__()
+        self.positional_embedder = PositionalEmbedder(embedding_dim)
+        self.linear = nn.Linear(embedding_dim, codebook_size)
+        self.softmax = nn.Softmax(dim=1)
+        self.codebook = nn.Embedding(codebook_size, embedding_dim)
+        self.octree = Octree(0, 0, 0, 1)
+        self.features = []
+
+    def _search_node(self, node, x, y, z):
+        if node.children[0] is None:
+            # Node is a leaf, so return the data
+            return node.data
+        else:
+            # Node is not a leaf, so figure out which child to search in
+            index = 0
+            if x > node.x:
+                index += 1
+            if y > node.y:
+                index += 2
+            if z > node.z:
+                index += 4
+            if node.children[index] is None:
+                return None
+            return self._search_node(node.children[index], x, y, z)
+
+    def _search(self, node, x, y, z, distances):
+        if node.data is not None:
+            # Node is a leaf, so add the distance to the list
+            distances.append(np.linalg.norm([x - node.x, y - node.y, z - node.z]))
+        else:
+            # Node is not a leaf, so search its children
+            for child in node.children:
+                if child is not None:
+                    self._search(child, x, y, z, distances)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # Calculate distances to all nodes in the octree
+        distances = []
+        self._search(self.octree.root, x[:, 0], x[:, 1], x[:, 2], distances)
+        # Convert distances to probabilities using the softmax function
+        probs = self.softmax(torch.tensor(distances))
+        # Find the closest feature index in the codebook using the probabilities
+        _, indices = probs.max(dim=0)
+        # Return the corresponding feature vector from the codebook
+        return self.codebook(indices)
 
 
 class DensityHiddenEncoding(nn.Module):
