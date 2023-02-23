823b70f816848f116af58f8312791b7aafe7ba4d
Amanjyoti Mridha <quackingbob101@gmail.com>
2023-02-23 12:32:53 -0800

    update

---
+++ clip_utils.py
@@ -0,0 +1,187 @@
+import math
+from typing import Optional
+from absl import flags
+from functools import partial
+
+import jax
+from jax import random
+import jax.numpy as jnp
+from transformers import FlaxCLIPModel
+
+from nerf import utils
+
+FLAGS = flags.FLAGS
+
+@partial(jax.jit, static_argnums=[0])
+def semantic_loss(clip_model, src_image, target_embedding): 
+    c_image = utils.unshard(src_image[0])
+    f_image = utils.unshard(src_image[1])
+    w = int(math.sqrt(f_image.shape[0]))
+    c_image = c_image.reshape([w, w, 3])
+    f_image = f_image.reshape([w, w, 3])
+     
+    src_embedding = clip_model.get_image_features(pixel_values=preprocess_for_CLIP(jnp.stack([c_image,f_image],0).transpose(0, 3, 1, 2)))
+    src_embedding /= jnp.linalg.norm(src_embedding, axis=-1, keepdims=True)
+    sc_loss = 2 - jnp.sum(src_embedding * target_embedding)
+    return sc_loss, f_image
+
+def semantic_step_multi(render_pfn, clip_model, rng, state, batch):
+    '''
+    Semantic loss training step on multiple devices
+    This function composed of two parts, i.e., pmapped rendering function on multi devices and semantic loss function on a single device:
+    Note that rays can be sharded to each device, but rendered image can't
+
+    Args:
+        model: NeRF model
+        clip_model: CLIP_model
+        rng: jnp.ndarray, random number generator.
+        state: utils.TrainState, state of the model/optimizer.
+        batch: dict, a mini-batch of data for semantic information training
+        
+    Return:
+        sc_loss: float, semantic consistency loss to log
+        grad: gradients calculated by semantic_loss.
+        src_image: generated image from the random pose.
+    '''
+
+    random_rays = batch["random_rays"]
+    target_embedding = batch["embedding"]
+    rng, key_0, key_1 = random.split(rng,3)
+    
+    def loss_fn(variables):
+        images = render_pfn(variables, key_0, key_1, random_rays)
+        sc_loss, f_image = semantic_loss(clip_model, images, target_embedding)
+        return sc_loss * FLAGS.sc_loss_mult, f_image
+    (sc_loss, src_image), grad = jax.value_and_grad(loss_fn, has_aux = True)(jax.device_get(jax.tree_map(lambda x:x[0], state)).optimizer.target)
+    return sc_loss, grad, src_image
+
+@partial(jax.jit, static_argnums=[0, 1])
+def semantic_step_single(model, clip_model, rng, state, batch):
+    '''
+    Semantic loss training step on a single device
+
+    Args:
+        model: NeRF model
+        clip_model: CLIP_model
+        rng: jnp.ndarray, random number generator.
+        state: utils.TrainState, state of the model/optimizer.
+        batch: dict, a mini-batch of data for semantic information training
+        
+    Return:
+        sc_loss: float, semantic consistency loss to log
+        grad: gradients calculated by semantic_loss.
+        src_image: generated image from the random pose.
+    '''
+
+    random_rays = jax.tree_map(lambda x: x.reshape(-1,3), batch["random_rays"])
+    target_embedding = batch["embedding"]
+    rng, key_0, key_1 = random.split(rng,3)
+
+    def semantic_loss(variables):
+        c_image, f_image = model.apply(variables, key_0, key_1, random_rays, False, rgb_only = True)
+        w = int(math.sqrt(f_image.shape[0]))
+        c_image = c_image.reshape([w, w, 3])
+        f_image = f_image.reshape([w, w, 3])
+
+        src_embedding = clip_model.get_image_features(pixel_values=preprocess_for_CLIP(jnp.stack([c_image,f_image],0).transpose(0, 3, 1, 2)))
+        src_embedding /= jnp.linalg.norm(src_embedding, axis=-1, keepdims=True)
+        sc_loss = 2 - jnp.sum(src_embedding * target_embedding)
+        return sc_loss * FLAGS.sc_loss_mult, f_image
+
+    (sc_loss, src_image), grad = jax.value_and_grad(semantic_loss, has_aux = True)(jax.device_get(jax.tree_map(lambda x:x[0], state)).optimizer.target)
+    return sc_loss, grad, src_image
+
+
+def trans_t(t):
+    return jnp.array([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, t],
+        [0, 0, 0, 1]], dtype=jnp.float32)
+
+def rot_phi(phi):
+    return jnp.array([
+        [1, 0, 0, 0],
+        [0, jnp.cos(phi), jnp.sin(phi), 0],
+        [0,-jnp.sin(phi), jnp.cos(phi), 0],
+        [0, 0, 0, 1]], dtype=jnp.float32)
+
+def rot_theta(th):
+    return jnp.array([
+        [jnp.cos(th), 0,-jnp.sin(th), 0],
+        [0, 1, 0, 0],
+        [jnp.sin(th), 0, jnp.cos(th), 0],
+        [0, 0, 0, 1]], dtype=jnp.float32)
+
+def pose_spherical(radius, theta, phi):
+    '''
+        The codes for generating random pose is based on https://github.com/yenchenlin/nerf-pytorch/blob/ec26d1c17d9ba2a897bc2ab254a0e15fce0d83b8/load_LINEMOD.py.
+        and modified to make each coordinate indicates below.
+
+               theta
+                 ^
+        - phi <= o => phi 
+                /
+            radius 
+
+        Args:
+            components of spharical coordinates.
+    '''
+    c2w = trans_t(radius)
+    c2w = rot_phi(phi) @ c2w
+    c2w = rot_theta(theta) @ c2w
+    c2w = jnp.array([[-1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]]) @ c2w
+    return c2w
+
+def random_pose(rng, bds):
+    '''
+    sample random poses in the upper hemisphere.
+
+    Args:
+        rng: jnp.ndarray, random number generator.
+        bds: boundaries of radious
+
+    Return:
+        Translation-rotation matrix according to a random pose:
+    '''
+    rng, *rng_inputs = jax.random.split(rng, 3)
+    radius = random.uniform(rng_inputs[1], minval=bds[0], maxval=bds[1])
+    theta = random.uniform(rng_inputs[1], minval=-jnp.pi, maxval=jnp.pi)
+    phi = random.uniform(rng_inputs[1], minval=0, maxval=jnp.pi/2)
+    return pose_spherical(radius, theta, phi)
+
+def preprocess_for_CLIP(image):
+    """
+        jax-based preprocessing for CLIP
+        Args:
+            image [B, 3, H, W]: batch image
+        Return
+            image [B, 3, 224, 224]: pre-processed image for CLIP
+    """
+    B, D, H, W = image.shape
+    mean = jnp.array([0.48145466, 0.4578275, 0.40821073]).reshape(1, 3, 1, 1)
+    std = jnp.array([0.26862954, 0.26130258, 0.27577711]).reshape(1, 3, 1, 1)
+    image = jax.image.resize(image, (B, D, 224, 224), 'bicubic')  # assume that images have rectangle shape.
+    image = (image - mean.astype(image.dtype)) / std.astype(image.dtype)
+    return image
+
+def init_CLIP(dtype: str, model_name: Optional[str]) -> FlaxCLIPModel:
+    """
+        initialize jax-based CLIP model
+        Args:
+            dtype: string, data type of CLIP
+            model_name: string, CLIP model's name
+        Return
+            CLIP model
+    """
+    if dtype == 'float16':
+        dtype = jnp.float16
+    elif dtype == 'float32':
+        dtype = jnp.float32
+    else:
+        raise ValueError
+
+    if model_name is None:
+        model_name = 'openai/clip-vit-base-patch32'
+
+    return FlaxCLIPModel.from_pretrained(model_name, dtype=dtype)
---
+++ train.py
@@ -0,0 +1,320 @@
+import functools
+import gc
+import time
+from absl import app
+from absl import flags
+import flax
+from flax.metrics import tensorboard
+from flax.training import checkpoints
+import jax
+from jax import config
+from jax import random
+import jax.numpy as jnp
+import numpy as np
+# import wandb
+from tqdm import tqdm
+
+from nerf import datasets
+from nerf import models
+from nerf import utils
+from nerf import clip_utils
+
+FLAGS = flags.FLAGS
+
+utils.define_flags()
+config.parse_flags_with_absl()
+
+# set up TPU for colab
+import os
+if "COLAB_TPU_ADDR" in os.environ:
+    import jax.tools.colab_tpu
+    jax.tools.colab_tpu.setup_tpu()
+print(f"detected device: {jax.local_devices()}")
+
+
+def train_step(model, rng, state, batch):
+    """One forward and back propagation step for main loss.
+    Args:
+        model: The linen model.
+        rng: jnp.ndarray, random number generator.
+        state: utils.TrainState, state of the model/optimizer.
+        batch: dict, a mini-batch of data for training.
+        
+    Returns:
+        gradients: grdients caculated by main loss.
+        stats: list. [(loss, psnr), (loss_coarse, psnr_coarse)].
+        rng: jnp.ndarray, updated random number generator.
+    """
+    rng, key_0, key_1 = random.split(rng, 3)
+
+    def loss_fn(variables):
+        rays = batch["rays"]
+        ret = model.apply(variables, key_0, key_1, rays, FLAGS.randomized)
+        if len(ret) not in (1, 2):
+            raise ValueError(
+                "ret should contain either 1 set of output (coarse only), or 2 sets"
+                "of output (coarse as ret[0] and fine as ret[1]).")
+        # The main prediction is always at the end of the ret list.
+        rgb, unused_disp, unused_acc = ret[-1]
+        loss = ((rgb - batch["pixels"][Ellipsis, :3]) ** 2).mean()
+        psnr = utils.compute_psnr(loss)
+        if len(ret) > 1:
+            # If there are both coarse and fine predictions, we compute the loss for
+            # the coarse prediction (ret[0]) as well.
+            rgb_c, unused_disp_c, unused_acc_c = ret[0]
+            loss_c = ((rgb_c - batch["pixels"][Ellipsis, :3]) ** 2).mean()
+            psnr_c = utils.compute_psnr(loss_c)
+        else:
+            loss_c = 0.
+            psnr_c = 0.
+
+        def tree_sum_fn(fn):
+            return jax.tree_util.tree_reduce(lambda x, y: x + fn(y),
+                                             variables, initializer=0)
+
+        weight_l2 = (tree_sum_fn(lambda z: jnp.sum(z ** 2)) /
+                     tree_sum_fn(lambda z: jnp.prod(jnp.array(z.shape))))
+
+        total_loss = loss + loss_c + FLAGS.weight_decay_mult * weight_l2
+        stats = utils.Stats(loss=loss, psnr=psnr, loss_c=loss_c,
+                            psnr_c=psnr_c, weight_l2=weight_l2)
+        return total_loss, stats
+
+    (_, stats), grad = (
+        jax.value_and_grad(loss_fn, has_aux=True)(state.optimizer.target))
+    stats = jax.lax.pmean(stats, axis_name="batch")
+    
+    # Clip the gradient by value.
+    if FLAGS.grad_max_val > 0:
+        clip_fn = lambda z: jnp.clip(z, -FLAGS.grad_max_val, FLAGS.grad_max_val)
+        grad = jax.tree_util.tree_map(clip_fn, grad)
+
+    # Clip the (possibly value-clipped) gradient by norm.
+    if FLAGS.grad_max_norm > 0:
+        grad_norm = jnp.sqrt(
+            jax.tree_util.tree_reduce(
+                lambda x, y: x + jnp.sum(y ** 2), grad, initializer=0))
+        mult = jnp.minimum(1, FLAGS.grad_max_norm / (1e-7 + grad_norm))
+        grad = jax.tree_util.tree_map(lambda z: mult * z, grad)
+
+    return grad, stats, rng
+
+def update_step(state, grad, lr):
+    grad = jax.lax.pmean(grad, axis_name="batch")
+    new_optimizer = state.optimizer.apply_gradient(grad, learning_rate=lr)
+    new_state = state.replace(optimizer=new_optimizer)
+    return new_state
+
+def main(unused_argv):
+    #wandb.init(project="hf-flax-clip-nerf", entity="wandb", sync_tensorboard=True)
+    rng = random.PRNGKey(20200823)
+    # Shift the numpy random seed by host_id() to shuffle data loaded by different
+    # hosts.
+    np.random.seed(20201473 + jax.host_id())
+
+    if FLAGS.config is not None:
+        utils.update_flags(FLAGS)
+    if FLAGS.batch_size % jax.device_count() != 0:
+        raise ValueError("Batch size must be divisible by the number of devices.")
+    if FLAGS.train_dir is None:
+        raise ValueError("train_dir must be set. None set now.")
+    if FLAGS.data_dir is None:
+        raise ValueError("data_dir must be set. None set now.")
+
+    # setup CLIP model
+    if FLAGS.use_semantic_loss:
+        clip_model = clip_utils.init_CLIP(FLAGS.clip_output_dtype,
+                                          FLAGS.clip_model_name)
+        print(f'semantic loss ACTIVATED, CLIP is set up '
+              f'(sc_loss_mult: {FLAGS.sc_loss_mult})')
+    else:
+        clip_model = None
+        print('semantic loss DEACTIVATED, CLIP is set to None')
+    
+    dataset = datasets.get_dataset("train", FLAGS, clip_model)
+    test_dataset = datasets.get_dataset("test", FLAGS, clip_model)
+
+    # setup NeRF model
+    rng, key = random.split(rng)
+    model, variables = models.get_model(key, dataset.peek(), FLAGS)
+    optimizer = flax.optim.Adam(FLAGS.lr_init).create(variables)
+    state = utils.TrainState(optimizer=optimizer)
+    del optimizer, variables
+    learning_rate_fn = functools.partial(
+        utils.learning_rate_decay,
+        lr_init=FLAGS.lr_init,
+        lr_final=FLAGS.lr_final,
+        max_steps=FLAGS.max_steps,
+        lr_delay_steps=FLAGS.lr_delay_steps,
+        lr_delay_mult=FLAGS.lr_delay_mult)
+
+    train_pstep = jax.pmap(
+        functools.partial(train_step, model),
+        axis_name="batch",
+        in_axes=(0, 0, 0),
+        donate_argnums=(2,))
+    
+    update_pstep = jax.pmap(
+        functools.partial(update_step,),
+        axis_name="batch",
+        in_axes=(0, 0, None),
+        donate_argnums=(0,))
+
+    def render_fn(variables, key_0, key_1, rays):
+        return model.apply(variables, key_0, key_1, rays, FLAGS.randomized)
+
+    render_pfn = jax.pmap(
+        render_fn,
+        in_axes=(None, None, None, 0),  # Only distribute the data input.
+        donate_argnums=(3,),
+        axis_name="batch")
+
+    def render_fn_(variables, key_0, key_1, rays):
+        # Render function for semantic loss paath.
+        # It only returns rgb
+        return model.apply(variables, key_0, key_1, rays, False, True)
+
+    render_pfn_ = jax.pmap(
+        render_fn_,
+        in_axes=(None, None, None, 0),  # Only distribute the data input.
+        donate_argnums=(3,),
+        axis_name="batch")
+
+    # Compiling to the CPU because it's faster and more accurate.
+    ssim_fn = jax.jit(
+        functools.partial(utils.compute_ssim, max_val=1.), backend="cpu")
+
+    if not utils.isdir(FLAGS.train_dir):
+        utils.makedirs(FLAGS.train_dir)
+    state = checkpoints.restore_checkpoint(FLAGS.train_dir, state)
+    # Resume training a the step of the last checkpoint.
+    init_step = state.optimizer.state.step + 1
+
+    # for distributive training
+    state = flax.jax_utils.replicate(state)
+    if jax.host_id() == 0:
+        summary_writer = tensorboard.SummaryWriter(FLAGS.train_dir)
+
+    # Prefetch_buffer_size = 3 x batch_size
+    pdataset = flax.jax_utils.prefetch_to_device(dataset, 3)
+    n_local_devices = jax.local_device_count()
+    rng = rng + jax.host_id()  # Make random seed separate across hosts.
+    keys = random.split(rng, n_local_devices)  # For pmapping RNG keys.
+    gc.disable()  # Disable automatic garbage collection for efficiency.
+    stats_trace = []
+    reset_timer = True
+
+    # for semantic loss update
+    sc_image = None
+    sc_loss = 0.
+    
+    for step, batch in tqdm(zip(range(init_step, FLAGS.max_steps + 1), pdataset)):
+        if reset_timer:
+            t_loop_start = time.time()
+            reset_timer = False
+        lr = learning_rate_fn(step)
+
+        # calculate gradients by main loss
+        grad, stats, keys = train_pstep(keys, state, batch)
+        
+        if step%FLAGS.sc_loss_every == 0 and FLAGS.use_semantic_loss and step < FLAGS.stop_sc_loss:
+            # calculate gradients by semantic loss at every FLAGS.sc_loss_every steps until step < FLAGS.stop_sc_loss.
+            sc_batch = dataset.get_clip_data(size = FLAGS.random_ray_size, downsample = FLAGS.random_ray_downsample)
+            
+            if jax.local_device_count() > 1:
+                # partially fit compiled function to allow rendering on multi-gpu
+                sc_loss, sc_grad, sc_image = clip_utils.semantic_step_multi(render_pfn_, clip_model, keys[0], state, sc_batch)
+            else:
+                # fully jit compiled function for a single device
+                sc_loss, sc_grad, sc_image = clip_utils.semantic_step_single(model, clip_model, keys[0], state, sc_batch)
+
+            leaves, treedef = jax.tree_flatten(grad)
+            sc_leaves, _ = jax.tree_flatten(sc_grad)
+            grad = treedef.unflatten(g+jnp.expand_dims(sc_g,0) for g, sc_g in zip(leaves, sc_leaves))
+        
+        state = update_pstep(state, grad, lr)
+
+        if jax.host_id() == 0:
+            stats_trace.append(stats)
+        if step % FLAGS.gc_every == 0:
+            gc.collect()
+
+        # Log training summaries. This is put behind a host_id check because in
+        # multi-host evaluation, all hosts need to run inference even though we
+        # only use host 0 to record results.
+        if jax.host_id() == 0:
+            if step % FLAGS.print_every == 0:
+                summary_writer.scalar("loss/train", stats.loss[0], step)
+                summary_writer.scalar("sc_loss", sc_loss, step)
+                summary_writer.scalar("psnr/train", stats.psnr[0], step)
+                summary_writer.scalar("train_coarse/loss", stats.loss_c[0], step)
+                summary_writer.scalar("train_coarse/psnr", stats.psnr_c[0], step)
+
+                avg_loss = np.mean(np.concatenate([s.loss for s in stats_trace]))
+                avg_psnr = np.mean(np.concatenate([s.psnr for s in stats_trace]))
+                stats_trace = []
+                summary_writer.scalar("train_avg/loss", avg_loss, step)
+                summary_writer.scalar("train_avg/psnr", avg_psnr, step)
+
+                steps_per_sec = FLAGS.print_every / (time.time() - t_loop_start)
+                reset_timer = True
+                rays_per_sec = FLAGS.batch_size * steps_per_sec
+                summary_writer.scalar("stats/weight_l2", stats.weight_l2[0], step)
+                summary_writer.scalar("stats/learning_rate", lr, step)
+                summary_writer.scalar("iter_speed/train_steps_per_sec", steps_per_sec, step)
+                summary_writer.scalar("iter_speed/train_rays_per_sec", rays_per_sec, step)
+                precision = int(np.ceil(np.log10(FLAGS.max_steps))) + 1
+                print(("{:" + "{:d}".format(precision) + "d}").format(step) +
+                      f"/{FLAGS.max_steps:d}: " + f"i_loss={stats.loss[0]:0.4f}, " +
+                      f"avg_loss={avg_loss:0.4f}, " +
+                      f"weight_l2={stats.weight_l2[0]:0.2e}, " +
+                      f"sc_loss={sc_loss:0.4f}, " +
+                      f"lr={lr:0.2e}, {rays_per_sec:0.0f} rays/sec")
+            if step % FLAGS.save_every == 0:
+                state_to_save = jax.device_get(jax.tree_map(lambda x: x[0], state))
+                checkpoints.save_checkpoint(
+                    FLAGS.train_dir, state_to_save, int(step), keep=100)
+
+        # Test-set evaluation.
+        if FLAGS.render_every > 0 and step % FLAGS.render_every == 0:
+            # We reuse the same random number generator from the optimization step
+            # here on purpose so that the visualization matches what happened in
+            # training.
+            t_eval_start = time.time()
+            eval_variables = jax.device_get(jax.tree_map(lambda x: x[0],
+                                                         state)).optimizer.target
+            test_case = next(test_dataset)
+            pred_color, pred_disp, pred_acc = utils.render_image(
+                functools.partial(render_pfn, eval_variables),
+                test_case["rays"],
+                keys[0],
+                FLAGS.dataset == "llff",
+                chunk=FLAGS.chunk)
+
+            # Log eval summaries on host 0.
+            if jax.host_id() == 0:
+                psnr = utils.compute_psnr(
+                    ((pred_color - test_case["pixels"]) ** 2).mean())
+                ssim = ssim_fn(pred_color, test_case["pixels"])
+                eval_time = time.time() - t_eval_start
+                num_rays = jnp.prod(jnp.array(test_case["rays"].directions.shape[:-1]))
+                rays_per_sec = num_rays / eval_time
+                summary_writer.scalar("iter_speed/test_rays_per_sec", rays_per_sec, step)
+                print(f"Eval {step}: {eval_time:0.3f}s., {rays_per_sec:0.0f} rays/sec")
+                summary_writer.scalar("psnr/test", psnr, step)
+                summary_writer.scalar("ssim/ssim", ssim, step)
+                if sc_image is not None:
+                    summary_writer .image("random_ray_image", sc_image, step)
+                summary_writer.image("test_pred_color", pred_color, step)
+                summary_writer.image("test_pred_disp", pred_disp, step)
+                summary_writer.image("test_pred_acc", pred_acc, step)
+                summary_writer.image("test_target", test_case["pixels"], step)
+
+    if FLAGS.max_steps % FLAGS.save_every != 0:
+        state = jax.device_get(jax.tree_map(lambda x: x[0], state))
+        checkpoints.save_checkpoint(
+            FLAGS.train_dir, state, int(FLAGS.max_steps), keep=100)
+
+
+if __name__ == "__main__":
+    app.run(main)
