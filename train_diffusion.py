468bf74b73d3fbbb75bcb47c40e517dd2af60fd6
Amanjyoti Mridha <71572787+QuackingBob@users.noreply.github.com>
2023-02-27 00:19:29 -0800

    add diffusion models

--- train_diffusion.py
+++ train_diffusion.py
@@ -1,9 +1,15 @@
 import numpy as np
 import cv2
 import glob
-import tqdm
+from tqdm import tqdm
 import wandb
 from einops import rearrange
+from unet import Unet
+import torch
+import os
+from sklearn.model_selection import train_test_split
+from copy import deepcopy
+
 
 def apply_gaussian_noise(image, num_times, mean=0, std=1):
     """
@@ -20,8 +26,7 @@ def apply_gaussian_noise(image, num_times, mean=0, std=1):
         noisy_image = cv2.addWeighted(noisy_image, 0.5, noise, 0.5, 0)
 
 
-    noisy_image = np.clip(noisy_image, 0, 255)
-    noisy_image = noisy_image.astype(np.uint8)
+    noisy_image = np.clip(noisy_image, 0, 1)
 
     return noisy_image
 
@@ -30,33 +35,40 @@ w = 512
 h = 512
 max_time = 5
 
-image_paths = []
-depth_map = []
+image_paths = glob.glob('[path to rbg images]/*.[file type]')
+depth_map = glob.glob('[path to depth images]/*.[file type]')
 
 data = []
+sample_image = None
 
-for i in range(len(image_paths)):
+for i in range(5):# range(len(image_paths)):
     img = cv2.imread(image_paths[i])
-    depthmap = cv2.imread(y[i])
+    depthmap = cv2.imread(depth_map[i], cv2.IMREAD_GRAYSCALE)
 
     # resize image and depthmap to desired dimensions
     img_resized = cv2.resize(img, (w, h))
+    img_resized = np.divide(img_resized, 255.)
     img_resized = rearrange(img_resized, 'h w c -> c h w')
+    if sample_image is None:
+        sample_image = img_resized
     depthmap_resized = cv2.resize(depthmap, (w, h))
+    depthmap_resized = np.divide(depthmap_resized, 255.)
 
     t = max_time
 
     for i in range(t):
         if t-i == 1:
-            prev_depthmap = np.zeros_like(depthmap_resized)
+            prev_depthmap = np.ones_like(depthmap_resized)
         else:
-            prev_depthmap = apply_gaussian_noise(depthmap_resized, 1)   
-        stacked = np.concatenate((img_resized, prev_depthmap), axis=2)
-        data.append((img, rearrange(depthmap_resized, 'h w c -> c h w'), t))
+            prev_depthmap = apply_gaussian_noise(depthmap_resized, 1)
+        stacked = np.concatenate((img_resized, rearrange(np.expand_dims(prev_depthmap, -1), 'h w c -> c h w')), axis=0)
+        data.append((stacked, rearrange(np.expand_dims(depthmap_resized, -1), 'h w c -> c h w'), t-1))
         depthmap_resized = prev_depthmap
 
 np.random.shuffle(data)
-img, depth, times = zip(*data)
+train_data, val_data = train_test_split(data, test_size=0.1, random_state=42)
+img, depth, times = zip(*train_data)
+img_val, depth_val, times_val = zip(*val_data)
 
 
 def batchify(x, y, t, batch_size):
@@ -70,3 +82,108 @@ def batchify(x, y, t, batch_size):
         t_batch.append(np.stack(t[i*batch_size:(i+1)*batch_size], axis=0))
     return (x_batch, y_batch, t_batch), num_batches
 
+
+name = "depth_diffusion"
+path = "diffusion/"
+
+if not os.path.exists(path + name + "/"):
+    os.mkdir(path+name)
+    os.mkdir(path+name+"/"+"checkpoints")
+
+model_save_path = path + name + "/" + f"{name}.pth"
+model_state_save_path = path + name + "/" + f"{name}_state.pth"
+checkpts_save_path = path + name + "/" + "checkpoints"
+
+wandb.init(project="depth_diffusion", name="diffusion_v1")
+
+batch_size = 64
+num_epochs = 100
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = Unet(device).to(device)
+mse_loss = torch.nn.MSELoss()
+optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, betas=(0.9, 0.999))
+
+train_data, train_batches = batchify(img, depth, times, batch_size)
+val_data, val_batches = batchify(img_val, depth_val, times_val, batch_size)
+
+test_img = rearrange(sample_image * 255.0, 'c h w -> h w c')
+wandb.log({"examples": [wandb.Image(test_img, caption="example image original")]})
+test_img_input = sample_image
+
+for epoch in range(num_epochs):
+
+    model.train()
+
+    pbar = tqdm(range(train_batches), desc=f"Epoch {epoch}")
+
+    train_loss = 0
+
+    for i in pbar:
+        x = torch.from_numpy(train_data[0][i])
+        y = torch.from_numpy(train_data[1][i])
+        t = torch.from_numpy(train_data[2][i])
+        x = x.to(device=device, dtype=torch.float)
+        y = y.to(device=device, dtype=torch.float)
+        t = t.to(device=device, dtype=torch.float)
+
+        optimizer.zero_grad()
+
+        y_pred = model(x, t)
+
+        loss = mse_loss(y_pred, y)
+
+        pbar.set_postfix({'loss': f"{loss.item():.4f}"})
+        train_loss = loss.item()
+
+        loss.backward()
+        optimizer.step()
+    
+    model.eval()
+
+    with torch.no_grad():
+        x = np.concatenate((test_img_input, np.ones_like(depth[0])), axis=0)
+        x = np.expand_dims(x, axis=0)
+        x = torch.from_numpy(x).to(device=device, dtype=torch.float)
+
+        for i in range(max_time):
+            time_input = np.array([i])
+            time_input = torch.from_numpy(time_input).to(device=device, dtype=torch.float)
+            test_img_pred = model(x, time_input)
+            x = np.concatenate((test_img_input, test_img_pred.detach().cpu().numpy()[0]), axis=0)
+            x = np.expand_dims(x, axis=0)
+            x = torch.from_numpy(x).to(device=device, dtype=torch.float)
+        
+        test_img_rearranged = test_img_pred.detach().cpu().numpy()[0] * 255.0
+        test_img_rearranged = rearrange(test_img_rearranged, 'c h w -> h w c')
+        wandb.log({"outputs": [wandb.Image(test_img_rearranged, caption=f"depth at epoch {epoch}")]})
+
+        val_loss_num = 0
+
+        for i in range(val_batches):
+            x = torch.from_numpy(val_data[0][i])
+            y = torch.from_numpy(val_data[1][i])
+            t = torch.from_numpy(val_data[2][i])
+            x = x.to(device=device, dtype=torch.float)
+            y = y.to(device=device, dtype=torch.float)
+            t = t.to(device=device, dtype=torch.float)
+
+            y_pred = model(x, t)
+
+            val_loss = mse_loss(y_pred, y)
+
+            val_loss_num = val_loss.item()
+        
+        print(f"Eval: val_loss:{val_loss_num}")
+
+        wandb.log({"train":{"loss":train_loss}, "val":{"loss":val_loss_num}})
+    
+    if epoch % 10 == 9:
+        print(f"Saving checkpoint at epoch {epoch}")
+        torch.save(deepcopy(model.state_dict()), checkpts_save_path + f"/epoch_{epoch}_chkpt.pth")
+        torch.save(model.state_dict(), model_state_save_path)
+        torch.save(model, model_save_path)
+
+
+torch.save(model.state_dict(), model_state_save_path)
+torch.save(model, model_save_path)
--- unet.py
+++ unet.py
@@ -8,27 +8,32 @@ class Block(nn.Module):
         super(Block, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=(3,3), padding="same").to(device)
         self.conv2 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=(3,3), padding="same").to(device)
+        self.embed = SinusoidalPosEmb(out_channels).to(device)
         self.relu = nn.ReLU().to(device)
 
     
-    def forward(self, x):
+    def forward(self, x, time):
+        h, w = x.shape[2], x.shape[3]
         x = self.conv1(x)
         x = self.relu(x)
         x = self.conv2(x)
-        return x
+        t = self.embed(time)
+        t = t.unsqueeze(-1).unsqueeze(-1)
+        # x = torch.cat([x, t.expand(-1, -1, h, w)], dim=1)
+        return x + t
 
 
 class Encoder(nn.Module):
-    def __init__(self, device, channels=[3,64,128,256,512,1024]) -> None:
+    def __init__(self, device, channels=[4,64,128,256,512,1024]) -> None:
         super(Encoder, self).__init__()
         self.encoding_blocks = nn.ModuleList([Block(channels[i], channels[i+1], device).to(device) for i in range(len(channels) - 1)])
         self.pool = nn.MaxPool2d(2).to(device)
     
     
-    def forward(self, x):
+    def forward(self, x, time):
         features = []
         for block in self.encoding_blocks:
-            x = block(x)
+            x = block(x, time)
             features.append(x)
             x = self.pool(x)
         return features
@@ -42,31 +47,33 @@ class Decoder(nn.Module):
         self.deconv_blocks = nn.ModuleList([Block(channels[i+1], channels[i+1], device).to(device) for i in range(len(channels) - 1)])
     
 
-    def forward(self, x, encoder_features):
+    def forward(self, x, encoder_features, time):
         for i in range(len(self.channels) - 1):
             b, c, h, w = x.shape
             x = self.deconvs[i](x, output_size=(b, self.channels[i+1], 2*h, 2*w))
             x = encoder_features[i] + x
-            x = self.deconv_blocks[i](x)
+            x = self.deconv_blocks[i](x, time)
         return x
 
 
 class SinusoidalPosEmb(nn.Module):
-    def __init__(self, device, dim):
+    def __init__(self, dim):
         super(SinusoidalPosEmb, self).__init__()
-        self.device = device
         self.dim = dim
 
     def forward(self, time):
+        device = time.device
+        dtype = time.dtype
         half_dim = self.dim//2
-        emb = torch.log(10000) / (half_dim - 1)
-        emb = torch.exp(torch.arange(half_dim, dtype=self.dtype) * -emb)
-        emb = rearrange(time, "i -> i 1") * rearrange(emb, "j -> 1 j")
-        return torch.concatenate([torch.sin(emb), torch.cos(emb)], axis=-1, dtype=self.dtype)
+        emb = torch.log(torch.tensor(10000.0)) / (half_dim - 1)
+        emb = torch.exp(torch.arange(half_dim, dtype=dtype, device=device) * -emb)
+        emb = time.unsqueeze(-1) * emb.unsqueeze(0)
+        emb = torch.cat([torch.sin(emb), torch.cos(emb)], dim=-1)
+        return emb
 
 
 class Unet(nn.Module):
-    def __init__(self, device, inner_channels=[3,64,128,256,512,1024]) -> None:
+    def __init__(self, device, inner_channels=[4,64,128,256,512,1024]) -> None:
         super(Unet, self).__init__()
         self.device = device
         self.encoder_channels = inner_channels
@@ -76,13 +83,12 @@ class Unet(nn.Module):
         self.encoder = Encoder(device, self.encoder_channels).to(device)
         self.decoder = Decoder(device, self.decoder_channels).to(device)
         self.convoutput = nn.Conv2d(in_channels=inner_channels[1], out_channels=1, kernel_size=(1,1), padding="same").to(device)
-        self.sigmoid = nn.Sigmoid().to(device)
+        self.relu = nn.ReLU().to(device)
         
 
-    def forward(self, x):
-        encoder_filters = self.encoder(x)
-        x = self.decoder(encoder_filters[::-1][0], encoder_filters[::-1][1:])
+    def forward(self, x, t):
+        encoder_filters = self.encoder(x, t)
+        x = self.decoder(encoder_filters[::-1][0], encoder_filters[::-1][1:], t)
         x = self.convoutput(x)
-        if not self.training:
-            x = self.sigmoid(x)
+        x = self.relu(x)
         return x
